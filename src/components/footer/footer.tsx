2cd499199c9d868550779d52e03093b31da1972d
grazisouza1 <graziisouza0127@gmail.com>
2024-11-28 21:07:07 -0300

    feat: Add footer component

---
+++ src/components/footer/footer.tsx
@@ -0,0 +1,64 @@
+"use client";
+
+import Link from "next/link";
+import { PropsWithChildren } from "react";
+import { useForm } from "react-hook-form";
+import { Input } from "../input/input";
+import { Button } from "../button/button";
+
+const Text = ({ children }: PropsWithChildren) => {
+  return <p className="text-white text-center">{children}</p>;
+};
+
+export const Footer = () => {
+  const form = useForm({
+    defaultValues: {
+      newsletterEmail: "",
+    },
+  });
+
+  const onSubmit = async (data: { newsletterEmail: string }) => {
+    console.log(data);
+    form.reset();
+  };
+
+  return (
+    <footer className="p-8 bg-blue-500 flex flex-col-reverse gap-4 lg:flex-row lg:justify-evenly lg:items-center">
+      <div>
+        <Text>
+          Av. Eng. Fábio Roberto Barnabé, 2800 - M.D. - CEP: 13331-900
+        </Text>
+        <Text>Telefones: (19)3834-9000 / 0800-770-7702</Text>
+        <Text>
+          Mais informações em:{" "}
+          <Link
+            href="https://www.indaiatuba.sp.gov.br/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-blue-800"
+          >
+            https://www.indaiatuba.sp.gov.br/
+          </Link>
+        </Text>
+      </div>
+      <form onSubmit={form.handleSubmit(onSubmit)}>
+        <div className="input-container">
+          <Input
+            type="email"
+            id="newsletterEmail"
+            label="Receba atualizações:"
+            labelStyle="text-white"
+            placeholder="Digite seu e-mail"
+            {...form.register("newsletterEmail")}
+          />
+        </div>
+        <Button
+          type="submit"
+          variant="secondary"
+          className="mt-2"
+          label="Enviar"
+        />
+      </form>
+    </footer>
+  );
+}
